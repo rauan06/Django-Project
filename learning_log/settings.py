c322a15c4f4afd58026712a7caa5f4422bf6dcb0
rauan06 <ommirkulr@gmail.com>
2024-02-19 12:45:16 +0600

    Added home page

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,9 +1,6 @@
-"""Defines URL patterns for learning_logs."""
-
 from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    # Homepage
-    re_path(r'^$', views.index, name='index')
+    re_path(r'^$', views.index, name='index'),
 ]
