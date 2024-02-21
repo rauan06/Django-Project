41eb2068dd114a1cc1a27069b88af1b22a56f816
rauan06 <ommirkulr@gmail.com>
2024-02-21 10:44:23 +0600

    Added 2 pages

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, re_path
 
+app_name = "learning_logs"
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
