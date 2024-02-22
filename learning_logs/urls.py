f4fad963dc23e458db76a5816cabf90b50207703
rauan06 <ommirkulr@gmail.com>
2024-02-22 09:00:36 +0600

    Small updates

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     re_path(r'^$', views.index, name='index'),
     re_path(r'^topics/$', views.topics, name = "topics"),
     re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name = "topic"),
+    re_path(r'^new_topic/$', views.new_topic, name = 'new_topic')
 ]
