00623c0763e99e09bc4f14ff82becbcae7074355
rauan06 <ommirkulr@gmail.com>
2024-02-24 16:00:35 +0600

    added login page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name = 'topic'),
     re_path(r'^new_topic/$', views.new_topic, name = 'new_topic'),
     re_path(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name = 'new_entry'),
-    re_path(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name = "edit_entry")
+    re_path(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name = 'edit_entry'),
 ]
