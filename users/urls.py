58beac01a3ea86cf8f0dcd3300d03ac35b89c27e
rauan06 <ommirkulr@gmail.com>
2024-02-24 16:56:33 +0600

    Small changes

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,9 @@ app_name = 'users'
 
 urlpatterns = [
     # Login Page
-    re_path(r'^login/$', LoginView.as_view(template_name='users/login.html'), name = 'login'),   
+    re_path(r'^login/$', LoginView.as_view(template_name='users/login.html'), name = 'login'), 
+    # Logout Page
+    re_path(r'^logout/$', views.logout_view, name='logout'),
+    # Register Page
+    re_path(r'^register/$', views.register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,30 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def logout_view(request):
+    """Logout"""
+    logout(request)
+    return render(request, 'learning_logs/index.html')
+
+def register(request):
+    """Register a new user"""
+    if request.method != 'POST':
+        # Display registration form
+        form = UserCreationForm()
+    else:
+        """Form was filled"""
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Log the user in and rediret to home page
+            auth_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            login(request, auth_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+
+    context = {'form' : form}
+    return render(request, 'users/register.html', context)
